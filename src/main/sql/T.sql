a03529866c93d6b2d99b7bed0d9fc6d520099cb7
Tiago Martinho <80893376+tiagomartinhoo@users.noreply.github.com>
2023-04-28 15:58:49 +0100

    (f) done, (i) with error

--- src/main/sql/T.sql
+++ src/main/sql/T.sql
@@ -1,32 +1,56 @@
 
 -- ############################ EX f ################################
 
-create or replace function totalJogosJogador(user_id int, OUT games_count int) 
-language plpgsql
-As
-$$
+CREATE OR REPLACE FUNCTION totalJogosJogador(p_id INT)
+	RETURNS INT AS $$
+DECLARE
+	games_count INT;
+BEGIN
+	SELECT COUNT(DISTINCT game_id) INTO games_count
+	FROM player_score AS p
+	WHERE p.player_id = p_id;
+	RETURN games_count;
+END;
+$$ LANGUAGE plpgsql;
 
-begin
-  -- TODO: obter a contagem das entradas em MATCH com game_id unico
-  
-  	return games_count;
-  
-end;$$;
-
-
-
+SELECT totalJogosJogador(2);
 
 
 -- ############################ EX i ################################
 
-create or replace procedure iniciarConversa(user_id int, c_name text, OUT c_id int)
-language plpgsql
-As
-$$
+CREATE OR REPLACE PROCEDURE iniciarConversa (
+	p_id INT,
+	chat_name TEXT,
+	OUT c_id INT
+)
+	LANGUAGE plpgsql
+AS $$
+DECLARE
+	player_name VARCHAR;
+BEGIN
+	-- insere a conversa na tabela de chat e retorna o id
+	INSERT INTO chat (c_name)
+	VALUES (chat_name)
+	RETURNING id INTO c_id;
 
-begin
-	-- TODO
-end;$$;
+	-- insere o player na tabela de chat_lookup
+	INSERT INTO chat_lookup (chat_id, player_id)
+	VALUES (c_id, p_id);
+
+	-- obtém o name do player que iniciou a conversa
+	SELECT username INTO player_name
+	FROM player
+	WHERE id = p_id;
+
+	-- insere uma mensagem na conversa informando que o jogador criou a conversa
+	INSERT INTO message
+	VALUES (1, c_id, p_id, NOW(), CONCAT('The player ', player_name, ' starts the chat.'));
+
+END;
+$$;
+
+CALL iniciarConversa(1, CAST('Test' AS TEXT));
+
 
 -- ############################ EX l ################################
 
